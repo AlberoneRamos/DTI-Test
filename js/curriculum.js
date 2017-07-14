a05423ed480f0deb66194d5f32360b14436aa842
henriqueramos <henrique.ramos@teknisa.com>
2017-07-14 04:46:57 +0000

    Acertos

--- js/curriculum.js
+++ js/curriculum.js
@@ -13,6 +13,7 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var linkedIn = $("input#linkedIn").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -35,7 +36,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Currículum enviado! Agora é só esperar! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
